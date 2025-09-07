19697755c7699f834bf8a8884de3ce9ef501a736
RA2211003010031 <ar6893@srmist.edu.in>
2025-09-07 07:32:37 +0530

    removing duplicates

--- main.tf
+++ main.tf
@@ -1,23 +1,8 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = var.aws_region
-}
-
 # VPC
 resource "aws_vpc" "main" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
-  tags = {
-    Name = "book-bazaar-vpc"
-  }
+  tags = { Name = "book-bazaar-vpc" }
 }
 
 # Subnet
@@ -26,32 +11,24 @@ resource "aws_subnet" "public" {
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
   availability_zone       = "${var.aws_region}a"
-  tags = {
-    Name = "book-bazaar-subnet"
-  }
+  tags = { Name = "book-bazaar-subnet" }
 }
 
-# Internet Gateway
+# Internet Gateway + Route Table
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
-  tags = {
-    Name = "book-bazaar-igw"
-  }
+  tags   = { Name = "book-bazaar-igw" }
 }
 
-# Route Table
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
   }
-  tags = {
-    Name = "book-bazaar-rt"
-  }
 }
 
-resource "aws_route_table_association" "public_assoc" {
+resource "aws_route_table_association" "assoc" {
   subnet_id      = aws_subnet.public.id
   route_table_id = aws_route_table.public.id
 }
@@ -67,46 +44,28 @@ resource "aws_security_group" "sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
-  tags = {
-    Name = "book-bazaar-sg"
-  }
 }
 
-# 3 EC2 Instances
+# EC2 Instances (3)
 resource "aws_instance" "app" {
   count         = 3
-  ami           = "ami-0f58b397bc5c1f2e8" # Ubuntu 22.04 LTS (Mumbai region)
+  ami           = "ami-0f58b397bc5c1f2e8" # Ubuntu 22.04 in ap-south-1
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.sg.id]
   key_name      = var.key_name
 
-  tags = {
-    Name = "book-bazaar-instance-${count.index + 1}"
-  }
-}
-
-# Variables
-variable "aws_region" {
-  default = "ap-south-1"
-}
-
-variable "key_name" {
-  description = "Name of your AWS key pair (without .pem extension)"
-  default     = "team-key-mumbai"
+  tags = { Name = "book-bazaar-instance-${count.index + 1}" }
 }
