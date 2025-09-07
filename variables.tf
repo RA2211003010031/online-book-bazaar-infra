bbad1c3d06b0446ce49b3c1f61fa35d53a2b2264
RA2211003010031 <ar6893@srmist.edu.in>
2025-09-07 07:21:51 +0530

    Review-1: VPC + 3 EC2 via Terraform Cloud

---
+++ variables.tf
@@ -0,0 +1,40 @@
+variable "aws_region" {
+  description = "AWS region to deploy to"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "vpc_cidr" {
+  description = "VPC CIDR"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+  description = "Public subnet CIDR"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "az" {
+  description = "Availability Zone"
+  type        = string
+  default     = "ap-south-1a"
+}
+
+variable "instance_count" {
+  description = "How many EC2 instances"
+  type        = number
+  default     = 3
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "key_name" {
+  description = "Existing EC2 key pair name"
+  type        = string
+}
